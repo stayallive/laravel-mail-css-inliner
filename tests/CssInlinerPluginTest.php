b9fbc073a83cac69c81ff532039b17d9e3217d9e
Maxime Maisonnas <30724805+Maxoulak@users.noreply.github.com>
2022-03-24 11:08:52 +0100

    Add support for `MixedPart` and `RelatedPart` (#2)

--- tests/CssInlinerPluginTest.php
+++ tests/CssInlinerPluginTest.php
@@ -12,7 +12,10 @@ use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mailer\Transport;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Part\AbstractPart;
 use Symfony\Component\Mime\Part\Multipart\AlternativePart;
+use Symfony\Component\Mime\Part\Multipart\MixedPart;
+use Symfony\Component\Mime\Part\Multipart\RelatedPart;
 use Symfony\Component\Mime\Part\TextPart;
 
 class CssInlinerPluginTest extends TestCase
@@ -25,9 +28,18 @@ class CssInlinerPluginTest extends TestCase
         'original-html-with-css',
         'original-html-with-link-css',
         'original-html-with-links-css',
+        'original-html-with-raw-image-base64',
         'converted-html',
         'converted-html-with-css',
         'converted-html-with-links-css',
+        'converted-html-with-raw-image-base64',
+    ];
+
+    private array $attachments;
+
+    private static array $attachmentDefinitions = [
+        'test.pdf',
+        'test.xlsx',
     ];
 
     public function setUp(): void
@@ -36,6 +48,14 @@ class CssInlinerPluginTest extends TestCase
             $this->stubs[$stub] = $this->cleanupHtmlStringForComparison(
                 file_get_contents(__DIR__ . "/stubs/{$stub}.stub")
             );
+        }
+
+        foreach (self::$attachmentDefinitions as $attachment) {
+            $this->attachments[$attachment] = \file_get_contents(implode(DIRECTORY_SEPARATOR, [
+                __DIR__,
+                'stubs',
+                $attachment
+            ]));
         }
     }
 
@@ -106,11 +126,84 @@ class CssInlinerPluginTest extends TestCase
         $this->assertBodyMatchesStub($message, 'converted-html-with-links-css');
     }
 
+    public function test_it_should_convert_html_body_with_attachments(): void
+    {
+        $message = $this->fakeSendMessageUsingInlinePlugin(
+            (new Email)->html($this->stubs['original-html'])
+                ->attach($this->attachments['test.pdf'], 'test.pdf', 'application/pdf')
+                ->attach($this->attachments['test.xlsx'], 'test.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        );
+
+        $this->assertBodyMatchesStub($message, 'converted-html');
+    }
+
+    public function test_it_should_convert_html_body_with_text_parts_and_attachments(): void
+    {
+        $message = $this->fakeSendMessageUsingInlinePlugin(
+            (new Email)->html($this->stubs['original-html'])
+                ->text($this->stubs['plain-text'])
+                ->attach($this->attachments['test.pdf'], 'test.pdf', 'application/pdf')
+                ->attach($this->attachments['test.xlsx'], 'test.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        );
+
+        $this->assertBodyMatchesStub($message, 'converted-html');
+        $this->assertBodyMatchesStub($message, 'plain-text', 'plain');
+    }
+
+    public function test_it_shloud_convert_html_body_with_raw_base64_image(): void
+    {
+        $message = $this->fakeSendMessageUsingInlinePlugin(
+            (new Email)->html($this->stubs['original-html-with-raw-image-base64'])
+        );
+
+        $this->assertBodyMatchesStub($message, 'converted-html-with-raw-image-base64');
+    }
+
+    public function test_it_shloud_convert_html_body_with_raw_base64_image_and_attachments(): void
+    {
+        $message = $this->fakeSendMessageUsingInlinePlugin(
+            (new Email)->html($this->stubs['original-html-with-raw-image-base64'])
+                ->attach($this->attachments['test.pdf'], 'test.pdf', 'application/pdf')
+                ->attach($this->attachments['test.xlsx'], 'test.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        );
+
+        $this->assertBodyMatchesStub($message, 'converted-html-with-raw-image-base64');
+    }
+
     private function assertBodyMatchesStub(object $message, string $stub, string $mediaSubType = 'html'): void
     {
         $this->assertInstanceOf(Email::class, $message);
 
         $body = $message->getBody();
+
+        if ($body instanceof MixedPart) {
+            $parts = $body->getParts();
+
+            foreach ($parts as $part) {
+                $actual = $this->getBodyFromSinglePart($part, $mediaSubType);
+
+                if ($actual !== null) {
+                    break;
+                }
+            }
+
+            if ($actual === null) {
+                throw new RuntimeException('Unable to find TextPart on MixedPart body');
+            }
+        } else {
+            $actual = $this->getBodyFromSinglePart($body, $mediaSubType);
+
+            if ($actual === null) {
+                throw new RuntimeException('Unknown message body type : ' . get_class($body));
+            }
+        }
+
+        $this->assertEquals($this->stubs[$stub], $this->cleanupHtmlStringForComparison($actual));
+    }
+
+    private function getBodyFromSinglePart(AbstractPart $body, string $mediaSubType = 'html'): ?string
+    {
+        $actual = null;
 
         if ($body instanceof TextPart) {
             $actual = $body->getBody();
@@ -118,11 +211,11 @@ class CssInlinerPluginTest extends TestCase
             $actual = (new Collection($body->getParts()))->first(
                 static fn ($part) => $part instanceof TextPart && $part->getMediaType() === 'text' && $part->getMediaSubtype() === $mediaSubType
             )->getBody();
-        } else {
-            throw new RuntimeException('Unknown message body type');
+        } elseif ($body instanceof RelatedPart) {
+            $actual = array_shift($body->getParts());
         }
 
-        $this->assertEquals($this->stubs[$stub], $this->cleanupHtmlStringForComparison($actual));
+        return $actual;
     }
 
     private function cleanupHtmlStringForComparison(string $string): string
